b90110e16f5af49b2289af92087d36e32efbbd5b
1-vlad-1 <vlad01022002@mail.ru>
2023-04-17 16:18:46 +0300

    fourth commit to third_branch

--- lab5.py
+++ lab5.py
@@ -14,7 +14,7 @@ def merge(dic1,dic2):
     dic3=dic1.copy()
     dic3.update(dic2)
     return dic3
-dic1={1:"hello", 2:"world"}
+dic1={1:"hello", 2:"world"}#fourth commit to third_branch
 dic2={3:"Python", 4:"Programming"}
 merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
 
