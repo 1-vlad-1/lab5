0014f239698567034da1c8160008328823a6feb8
1-vlad-1 <vlad01022002@mail.ru>
2023-04-17 16:09:12 +0300

    first commit to second_branch

--- lab5.py
+++ lab5.py
@@ -16,7 +16,7 @@ def merge(dic1,dic2):
     return dic3
 dic1={1:"hello", 2:"world"}
 dic2={3:"Python", 4:"Programming"}
-merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
+merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'} first commit to second branch
 
 import time
 start_time= time.time()
